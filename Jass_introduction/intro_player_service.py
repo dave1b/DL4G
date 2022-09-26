98de1f369aaf8109b72ee27704010f411b0c3705
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-09-26 18:11:19 +0200

    webservice with flask

---
+++ Jass_introduction/intro_player_service.py
@@ -0,0 +1,31 @@
+import logging
+
+from jass.service.player_service_app import PlayerServiceApp
+from Intro_Basic_Agent import *
+from jass.agents.agent_random_schieber import AgentRandomSchieber
+
+def create_app():
+    """
+    This is the factory method for flask. It is automatically detected when flask is run, but we must tell flask
+    what python file to use:
+        export FLASK_APP=player_service.py
+        export FLASK_ENV=development
+        flask run --host=0.0.0.0 --port=8888
+    """
+    logging.basicConfig(level=logging.DEBUG)
+
+    # create and configure the app
+    app = PlayerServiceApp('player_service')
+
+    # you could use a configuration file to load additional variables
+    # app.config.from_pyfile('my_player_service.cfg', silent=False)
+
+    # add some players
+    app.add_player('random', MyBasicAgent())
+
+    return app
+
+
+if __name__ == '__main__':
+   app = create_app()
+   app.run()
