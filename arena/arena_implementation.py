812d988c454448d64be89a1cdcfdc6ddef524fdf
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-10-26 20:34:16 +0200

    refactor

--- arena/arena_implementation.py
+++ arena/arena_implementation.py
@@ -1,15 +1,13 @@
-
 from jass.arena.arena import Arena
 from jass.game.game_util import *
 from jass.game.const import *
 from jass.agents.agent_random_schieber import AgentRandomSchieber
-
+from ml.mlp_agent import MLPOnlyTrumpAgent
 from intro_basic.set_only_trump_else_random_agent import SetOnlyTrumpElseRandomAgent
-#
 
 # Jass Arena for trying Agents
-arena = Arena(nr_games_to_play=100)
-arena.set_players(SetOnlyTrumpElseRandomAgent(), AgentRandomSchieber(), SetOnlyTrumpElseRandomAgent(), AgentRandomSchieber())
+arena = Arena(nr_games_to_play=1000)
+arena.set_players(MLPOnlyTrumpAgent(), AgentRandomSchieber(), MLPOnlyTrumpAgent(), AgentRandomSchieber())
 arena.play_all_games()
 # arena.play_game(dealer=NORTH)
 print(arena.points_team_0.sum(), arena.points_team_1.sum())
--- arena/arena_play_network.py
+++ arena/arena_play_network.py
@@ -3,6 +3,7 @@ import logging
 from jass.agents.agent_by_network import AgentByNetwork
 from jass.arena.arena import Arena
 from jass.agents.agent_random_schieber import AgentRandomSchieber
+from ml.mlp_agent import MLPOnlyTrumpAgent
 
 
 def main():
@@ -10,12 +11,13 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     # setup the arena
-    arena = Arena(nr_games_to_play=1)
+    arena = Arena(nr_games_to_play=50)
     player = AgentRandomSchieber()
-    my_player = AgentByNetwork('http://localhost:5000/SetOnlyTrumpElseRandomAgent')
-    #my_player = AgentByNetwork('https://lg3bsb3a96.execute-api.eu-central-1.amazonaws.com/dev/random')
+    my_player = AgentByNetwork('http://178.196.170.109:8888/MLPOnlyTrumpAgent')
+    my_player2 = AgentByNetwork('http://178.196.170.109:8888/AgentRandomSchieber')
+    # my_player = AgentByNetwork('https://lg3bsb3a96.execute-api.eu-central-1.amazonaws.com/dev/random')
 
-    arena.set_players(my_player, player, my_player, player)
+    arena.set_players(my_player, my_player2, my_player, my_player2)
     print('Playing {} games'.format(arena.nr_games_to_play))
     arena.play_all_games()
     print('Average Points Team 0: {:.2f})'.format(arena.points_team_0.mean()))
