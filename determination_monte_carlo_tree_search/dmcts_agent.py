de5d8c2e1fb7d3d572f919ab4eb8f5a22eb470d0
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-12-13 21:29:25 +0100

    refactor: concurrency improvements

--- determination_monte_carlo_tree_search/dmcts_agent.py
+++ determination_monte_carlo_tree_search/dmcts_agent.py
@@ -4,6 +4,7 @@ from jass.game.game_state_util import *
 from jass.game.const import *
 from jass.game.rule_schieber import RuleSchieber
 from jass.agents.agent import Agent
+from numpy import ndarray
 from determination_monte_carlo_tree_search.dmcts_node import DMCTSNode
 from concurrent.futures import ProcessPoolExecutor
 from collections import Counter
@@ -16,14 +17,13 @@ class DMCTSAgent(Agent):
         super().__init__()
         logging.basicConfig(level=logging.DEBUG)
         self.model_name = "mlp_model_v2"
+        # self.model_name = "./determination_monte_carlo_tree_search/mlp_model_v2"
         self._rule = RuleSchieber()
         self.round = 0
         self.thread_count = 22
         # self.thread_count = (min(32, (os.cpu_count() or 1) + 4))
-        self.thread_pool_executor = ProcessPoolExecutor(self.thread_count)
-        self._rng = np.random.default_rng()
-        self.time_budget_for_algorythm = 9
-        self.threads_running = False
+        self.time_budget_for_algorythm = 3
+        self.__dmcts_iteration_count = 3
         self.feature_columns = [
             # Diamonds
             'DA', 'DK', 'DQ', 'DJ', 'D10', 'D9', 'D8', 'D7', 'D6',
@@ -34,14 +34,14 @@ class DMCTSAgent(Agent):
             # Clubs
             'CA', 'CK', 'CQ', 'CJ', 'C10', 'C9', 'C8', 'C7', 'C6',
         ]
-        print("Thread count: ", self.thread_count)
+        logging.info("Thread count: %s", self.thread_count)
 
     def action_trump(self, game_observation: GameObservation) -> int:
         logging.info('Trump request mlp')
         # calculate if forehand
         forehand = int(game_observation.player_view == (game_observation.dealer + 3) % 4)
         hand = self._rule.get_valid_cards_from_obs(game_observation)
-        data = self.convert_hand_to_predict_form(hand, forehand)
+        data = self.__convert_hand_to_predict_form(hand, forehand)
         # load model
         mlp_model = keras.models.load_model(self.model_name)
         prediction = mlp_model.predict(data)[0]
@@ -54,24 +54,46 @@ class DMCTSAgent(Agent):
         logging.info("Round: %s ", self.round)
         self.round = self.round % 9
 
-        future_objects = []
-        for x in range(0, self.thread_count):
-            node = self.construct_root_node_threaded(game_observation)
-            future = self.thread_pool_executor.submit(node.best_action, self.time_budget_for_algorythm)
-            future_objects.append(future)
-        logging.info("%s threads running...", self.thread_count)
+        best_actions_list = []
+        # run dmcts
+        for i in range(0, self.__dmcts_iteration_count):
+            future_objects = []
+            process_pool_executor = ProcessPoolExecutor(max_workers=self.thread_count)
+            for x in range(0, self.thread_count):
+                node = self.__construct_root_node(game_observation)
+                future = process_pool_executor.submit(node.best_action, self.time_budget_for_algorythm)
+                future_objects.append(future)
+            logging.info("%s threads running...", self.thread_count)
+            best_actions_list += (list(map(lambda future_object: future_object.result(), future_objects)))
 
-        best_actions_list = list(map(lambda future_object: future_object.result(), future_objects))
+        # count best action
         counts = Counter(best_actions_list)
         best_action_overall = (max(counts.most_common(), key=lambda key_value: key_value[1]))[0]
         logging.info("Best actions: %s", best_actions_list)
         logging.info("Best overall action: %s", best_action_overall)
         logging.info("Best card to play: %s", card_strings[best_action_overall])
-        print("-------------------------------------------------")
+        logging.info("-------------------------------------------------")
         return best_action_overall
 
+    def __convert_hand_to_predict_form(self, hand, forehand: int):
+        np.transpose(hand)
+        forehand = pd.DataFrame(np.array([forehand]), columns=["FH"])
+        data = pd.DataFrame([hand], columns=self.feature_columns)
+        data = pd.concat([data, forehand], axis=1)
+        for color in 'DHSC':
+            # Jack and nine combination
+            new_col = '{}_J9'.format(color)
+            data[new_col] = data['{}J'.format(color)] & data['{}9'.format(color)]
+            # Ace King Queen combination
+            new_col = '{}_AKQ'.format(color)
+            data[new_col] = data['{}A'.format(color)] & data['{}K'.format(color)] & data['{}Q'.format(color)]
+            # Six seven eight combination
+            new_col = '{}_678'.format(color)
+            data[new_col] = data['{}6'.format(color)] & data['{}7'.format(color)] & data['{}8'.format(color)]
+        return data
+
     @staticmethod
-    def calculate_random_hands(game_obs: GameObservation):
+    def __calculate_random_hands(game_obs: GameObservation) -> ndarray:
         cards = np.arange(0, 36, dtype=np.int32)
         player_hand_int = convert_one_hot_encoded_cards_to_int_encoded_list(game_obs.hand)
         np.random.shuffle(cards)
@@ -94,29 +116,11 @@ class DMCTSAgent(Agent):
                 i += 1
         return hands
 
-    def construct_root_node_threaded(self, game_observation: GameObservation):
-        hands = self.calculate_random_hands(game_observation)
+    def __construct_root_node(self, game_observation: GameObservation) -> DMCTSNode:
+        hands = self.__calculate_random_hands(game_observation)
         game_state = state_from_observation(game_observation, hands)
         dmcts_node = DMCTSNode(game_state, game_observation.player_view)
         return dmcts_node
-
-    def convert_hand_to_predict_form(self, hand, forehand: int):
-        np.transpose(hand)
-        forehand = pd.DataFrame(np.array([forehand]), columns=["FH"])
-        print(forehand)
-        data = pd.DataFrame([hand], columns=self.feature_columns)
-        data = pd.concat([data, forehand], axis=1)
-        for color in 'DHSC':
-            # Jack and nine combination
-            new_col = '{}_J9'.format(color)
-            data[new_col] = data['{}J'.format(color)] & data['{}9'.format(color)]
-            # Ace King Queen combination
-            new_col = '{}_AKQ'.format(color)
-            data[new_col] = data['{}A'.format(color)] & data['{}K'.format(color)] & data['{}Q'.format(color)]
-            # Six seven eight combination
-            new_col = '{}_678'.format(color)
-            data[new_col] = data['{}6'.format(color)] & data['{}7'.format(color)] & data['{}8'.format(color)]
-        return data
 
 
 from jass.arena.arena import Arena
