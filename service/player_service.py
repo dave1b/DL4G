5f547c47964b383437acf4f0de0cc93d8a872a9f
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-12-13 21:36:40 +0100

    refactor: removed unused imports

--- service/player_service.py
+++ service/player_service.py
@@ -1,12 +1,10 @@
 import sys
 
 sys.path.insert(0, "C:/Users/Dave/Documents/GitHub/DL4G")
+# sys.path.insert(0, "/home/localadmin/DL4G")
 
 import logging
 from jass.service.player_service_app import PlayerServiceApp
-from jass.agents.agent_random_schieber import AgentRandomSchieber
-from intro_and_rule_based.set_only_trump_else_random_agent import SetOnlyTrumpElseRandomAgent
-from ML_sklearn.mlp_agent import MLPOnlyTrumpAgent
 from determination_monte_carlo_tree_search.dmcts_agent import DMCTSAgent
 
 
